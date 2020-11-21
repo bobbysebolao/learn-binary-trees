79915b109d881359320220fcf080a908f76ddc2b
Bobby <bobbysebolao@gmail.com>
2020-11-21 01:54:07 +0000

    Add method to reverse binary tree

--- binary_tree_refactored.js
+++ binary_tree_refactored.js
@@ -10,25 +10,37 @@ class TreeNode {
 
 class BinaryTree {
     constructor(arr) {
-        this.root = () => {
-            let rootNode = new TreeNode(this.values[0]);
+        this.getRoot = () => {
+            let rootNode = new TreeNode(this.getValues[0]);
             for (let i = 0; i < arr.length; i++) {
                 rootNode = this.insert(rootNode, arr[i]);
             }
             return rootNode;
         }
-                this.values = arr
+        this.getValues = arr
         this.insert = (node, arrValue) => {
                 if (!node) return new TreeNode(arrValue);
                 if (arrValue > node.value) node.right = this.insert(node.right, arrValue);
                 else if (arrValue < node.value) node.left = this.insert(node.left, arrValue);
                 return node;
+        }
+        this.reverse = (tree) => {
+            if (tree) {
+                const left = tree.left;
+                const right = tree.right;
+                tree.left = right;
+                tree.right = left;
+                this.reverse(tree.left);
+                this.reverse(tree.right);
             }
+            return tree;
+        }
     }
 }
 
 const tree = new BinaryTree([8,10,12,5,3,6]);
-// expected output of tree.root():
+console.log(tree.getRoot())
+// expected output of tree.getRoot():
 // {
 //    value:8,
 //    left:{
